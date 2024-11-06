728718ffe56ad3916e074da073ed43bb7d58aa34
Joe Küng <kuengjoe@students.zhaw.ch>
2024-11-06 10:29:04 +0100

    first approach indexing

---
+++ capzlog-ExtractDataFromPDF/util/indexing.cs
@@ -0,0 +1,43 @@
+﻿
+
+using UglyToad.PdfPig;
+
+namespace capzlog_ExtractDataFromPDF;
+
+public class Indexing
+{
+    private List<(int pageNumber, string firstLine)> _pageIndex;
+    private readonly PdfDocument _pdfDoc;
+
+    public  Indexing(PdfDocument pdfDoc)
+    {
+        _pdfDoc = pdfDoc;
+        _pageIndex = GetFirstLines();
+    }
+    
+    private List<(int pageNumber, string firstLine)> GetFirstLines()
+    {
+        List<(int pageNumber, string firstLine)> index = new List<(int pageNumber, string firstLine)>();
+        ExtractText extractText = new ExtractText();
+        var firstLines = extractText.ExtractFirstLines(_pdfDoc);
+        for (int i = 1; i <= _pdfDoc.NumberOfPages; i++)
+        {
+            
+            
+            index.Add((i, firstLines[i]));
+        }
+        return index;
+    }
+    
+    public int GetPageNumber(string firstLine)
+    {
+        foreach (var (pageNumber, line) in _pageIndex)
+        {
+            if (line == firstLine)
+            {
+                return pageNumber;
+            }
+        }
+        return -1;
+    }
+}
